e9bf2a2dfcb6d2ef665b37f95e3a55593e33a9a9
Fernando López Ramírez <39963559+Fernando74lr@users.noreply.github.com>
2020-08-12 23:25:20 -0500

    Update views.py

--- webPlayground/core/views.py
+++ webPlayground/core/views.py
@@ -5,7 +5,7 @@ class HomePageView(TemplateView):
     template_name = "core/home.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title':'My Super Web Playground'})
+        return render(request, self.template_name, {'title':'My Web PlayGround Made With Django'})
 
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
